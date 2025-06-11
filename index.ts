e205c390f1c4b8b89c46edf35c030d8ad015db03
Abdi-01 <abdialghi@gmail.com>
2025-06-11 20:02:45 +0700

    add product API

--- index.ts
+++ index.ts
@@ -20,21 +20,6 @@ interface IProduct {
 // Define req methode and url
 server.use("/products", productsRouter);
 
-server.post("/products", (req: Request, res: Response) => {
-    console.log(req.body);
-
-    // 1. Mengakses data sebelumnya pada db.json
-    const data = JSON.parse(fs.readFileSync("./db.json").toString());
-    // 2. Menambahkan data baru ke array of object product
-    data.products.push(req.body);
-    // 3. Menulis ulang isi dari db.json dengan data terbaru menggunakan fs.writeFileSync()
-    fs.writeFileSync("./db.json", JSON.stringify(data, null, 2));
-    // 4. Kirim response dengan pesan Add data berhasil
-    res.status(201).send({
-        message: "Add data success"
-    })
-});
-
 server.get("/categories", (req: Request, res: Response) => {
     const data = JSON.parse(fs.readFileSync("./db.json").toString());
 
--- routers/products.router.ts
+++ routers/products.router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getProductbyName, getProducts } from "../controllers/products.controller";
+import { addProduct, getProductbyName, getProducts } from "../controllers/products.controller";
 
 const router = Router();
 
 router.get("/data", getProducts);
-router.get("/:name", getProductbyName)
-
+router.get("/:name", getProductbyName);
+router.post("/add", addProduct);
 export default router;
 
